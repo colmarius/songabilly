1e42cd512d9e8dcb6da5af40ea79fe65602a81b0
Marius Colacioiu <marius.colacioiu@gmail.com>
2013-10-19 12:24:32 +0200

    Add initial Rails scaffold.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpazzaRubini::Application.config.secret_key_base = 'ac6c519e1dcc58d77e3208bd7d187f7ba784f440bd7d5b55c0e8dd32b2bac272843a6246bb1c455e4cd0e5330ca56132fad3c4ab8ff33289dc9d48e84387cd37'
