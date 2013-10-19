dddee33503f8f8fa1075c018032a4a985b02fade
Marius Colacioiu <marius.colacioiu@gmail.com>
2013-10-20 01:41:37 +0200

    Logic moved to GameEntry. Duilio is preparing this model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,2 @@
 class Game
-  attr_reader :id, :tracks
-
-  def initialize
-    @tracks = Track.generate(10)
-    @id = @tracks.first.id
-  end
-
-  def to_json
-    {
-      id: id,
-      tracks: tracks
-    }
-  end
 end
