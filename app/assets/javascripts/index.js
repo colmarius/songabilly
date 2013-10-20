21f2dabb6d4cb41b7505c9608363b01f14efd849
Ignazio Setti <ignazio.setti@gmail.com>
2013-10-20 02:55:02 +0200

    Make index view a bit prettier

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('[name=genre]').typeahead({
     name: 'trends' + Math.random(),
     prefetch: '/api/genres.json',
-    valueKey: 'name'
+    valueKey: 'name',
+    limit: 10
   });
 });
