b166831cd265bb30a106a616dd720e6c7b685414
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2013-10-20 05:11:31 +0200

    add method to check if answer is correct

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,4 +3,8 @@ class Quiz < ActiveRecord::Base
   belongs_to :game
   serialize :options, Array
   serialize :right_answer
+
+  def correct?(answer_id)
+    right_answer.echonest_track_id == answer_id
+  end
 end
